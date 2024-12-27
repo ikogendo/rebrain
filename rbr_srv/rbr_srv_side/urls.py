667dd9a2242529fff001696d6e4a5bf057d3388c
NikG <it_kcrb@mail.ru>
2024-12-27 22:45:54 +0200

    lesson 14 working state

---
+++ rbr_srv/rbr_srv_side/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .views import ServerViewSet, ServerDetailView, ServerAddView
+
+
+urlpatterns = [
+    path('servers/', ServerViewSet.as_view()),
+    path('servers/<int:pk>', ServerDetailView.as_view()),
+    path('servers/add', ServerAddView.as_view()),
+]
--- rbr_srv/rbr_srv_side/views.py
+++ rbr_srv/rbr_srv_side/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .serializer import ServerSerializer
+from .models import Server
+
+class ServerViewSet(generics.ListAPIView):
+
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
+
+class ServerAddView(generics.CreateAPIView):
+
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
+
+class ServerDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
