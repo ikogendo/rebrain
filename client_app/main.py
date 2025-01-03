2138585738899831f1d7ce7d4c47bd1643e20515
WorkPC <trash@gof*.off>
2025-01-03 16:21:27 +0200

    add  main  script for client side  app

---
+++ client_app/main.py
@@ -0,0 +1,12 @@
+import psutil
+import requests
+import os
+
+list_psutil=['host_info','network','disk','memory','cpu','load']
+
+host_info=['os','hostname','boot_time']
+network=['list','interface','mtu']
+disk=['disk','mountpoint','fs','used','total']
+memory=['mem_total','mem_used','mem_percent']
+cpu=['cpu_cores','cpu_phys','cpu_freq']
+load=['1 min','5 min', '15 min']
